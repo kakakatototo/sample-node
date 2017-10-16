bf16bd12a0bbb037ed8e90f924fb4979b06682c8
kakakatototo <katouyuuki@gmail.com>
2017-10-17 00:01:26 +0900

    eslintrc

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,7 @@ gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'));
-})
+});
  
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
